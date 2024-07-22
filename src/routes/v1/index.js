0e7f433893c20f9f4402ccbfc980289865d05e38
KeshavTandon <keshavtandon51@gmail.com>
2024-07-22 10:53:52 +0530

    Added isAuthenticated middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
-const { InfoController, UserController } = require("../../controllers");
-const userRouter=require('./user-routes');
+const { checkAuth } = require("../../middlewares/Auth-middleware");
+const { InfoController } = require("../../controllers");
+const userRouter = require("./user-routes");
 const router = express.Router();
 
-router.get("/info", InfoController.info);
+router.get("/info", checkAuth, InfoController.info);
 router.use("/user", userRouter);
 
 module.exports = router;
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -36,7 +36,6 @@ async function signin(data) {
       );
     }
     const passwordMatch = Auth.checkPassword(data.password, user.password);
-    console.log(passwordMatch);
 
     if (!passwordMatch) {
       throw new AppError("Invalid password", StatusCodes.BAD_REQUEST);
@@ -54,4 +53,31 @@ async function signin(data) {
   }
 }
 
-module.exports = { createUser, signin };
+async function isAuthenticated(token) {
+  try {
+    if (!token) {
+      throw new AppError("Missing JWT token", StatusCodes.BAD_REQUEST);
+    }
+    const response = Auth.verifyToken(token);
+    const user = await userRepository.get(response.id);
+    if (!user) {
+      throw new AppError("No user found", StatusCodes.NOT_FOUND);
+    }
+    return user.id;
+  } catch (error) {
+    if (error instanceof AppError) throw error;
+    if (error.name == "JsonWebTokenError") {
+      throw new AppError("Invalid JWT token", StatusCodes.BAD_REQUEST);
+    }
+    if (error.name == "TokenExpiredError") {
+      throw new AppError("JWT token expired", StatusCodes.BAD_REQUEST);
+    }
+    console.log(error);
+    throw new AppError(
+      "Something went wrong",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+module.exports = { createUser, signin ,isAuthenticated };
